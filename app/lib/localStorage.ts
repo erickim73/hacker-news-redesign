7a75439dc5c39a455ee98ed44d095f7de4774546
Eric Kim <seyoon2006@gmail.com>
2025-03-07 16:43:53 -0600

    added local storage

---
+++ app/lib/localStorage.ts
@@ -0,0 +1,57 @@
+import {useState, useEffect} from 'react'
+
+// custom hook for using local storage for data
+export function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T) => void] {
+    const [storedValue, setStoredValue] = useState<T>(() => {
+        if (typeof window === 'undefined') {
+            return initialValue
+        } try {
+            // get from local storage with key
+            const item = window.localStorage.getItem(key)
+            return item? JSON.parse(item) : initialValue
+        } catch (error) {
+            console.error('Error reading local storage key "${key}', error)
+            return initialValue
+        }
+    })
+
+    // function that sets value of data in local storage
+    const setValue = (value: T) => {
+        try {
+            // value can be a function  so we have same api as useState
+            const valueToStore = value instanceof Function ? value(storedValue) : value
+
+            // save state
+            setStoredValue(valueToStore)
+
+            // save to local storage
+            if (typeof window !== 'undefined') {
+                window.localStorage.setItem(key, JSON.stringify(valueToStore))
+            }
+        } catch (error) {
+            console.error(`Error setting localStorage key "${key}": `, error)
+        }
+    }
+    
+    // change state if local storage changes in another tab
+    useEffect(() => {
+        function handleStorageChange(event: StorageEvent) {
+            if (event.key === key) {
+                try {
+                    const newValue = event.newValue ? JSON.parse(event.newValue) : initialValue
+                    setStoredValue(newValue)
+                } catch (error) {
+                    console.error(`Error parsing localStorage change for key "${key}":`, error)
+                }
+            }
+        }
+
+        // listen for changes to local storage
+        if (typeof window !== 'undefined') {
+            window.addEventListener('storage', handleStorageChange)
+            return () => window.removeEventListener('storage', handleStorageChange)
+        }
+    }, [key, initialValue])
+
+    return [storedValue, setValue]
+}
